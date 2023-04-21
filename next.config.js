f8c6f43d0fc3a3eb01b969562356b997a8b385b7
SHASHANK <shashank7016204503@gmail.com>
2023-04-21 10:15:02 +0530

    Basic IPFS

--- next.config.js
+++ next.config.js
@@ -3,4 +3,12 @@ const nextConfig = {
   reactStrictMode: true,
 }
 
-module.exports = nextConfig
+module.exports = {
+  
+  
+  nextConfig,
+  env:{
+    NEXT_PROJECT_ID:process.env.NEXT_PROJECT_ID,
+    NEXT_Project_Secret_Key:process.env.NEXT_Project_Secret_Key
+  }
+}
